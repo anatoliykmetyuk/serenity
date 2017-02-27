ad0cd734baea64f1e0f9cd007f1c72635b5acc6e
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2017-02-27 22:04:46 +0200

    Layouts

--- src/main/scala/serenity/Main.scala
+++ src/main/scala/serenity/Main.scala
@@ -11,7 +11,7 @@ import cats.instances.all._
 import fs2.{ Task, Stream, text }
 
 import io.circe.yaml.{ parser => yamlParser }
-import io.circe.{ Json, ParsingFailure }
+import io.circe.{ Json, ParsingFailure, JsonObject }
 
 object Main {
   val workdirPath = "workdir"
@@ -33,6 +33,7 @@ object Main {
     yamlParser.parse {"""
     |posts: _posts/
     |includes: _includes/
+    |layouts: _layouts/
     """.stripMargin}
     .leftMap(_.getMessage)
   })
@@ -50,6 +51,7 @@ object Main {
 
   // TODO: Separate IO plugin
   def readFile(path: String): Stuff[String] = EitherT(Eval.later {
+    println(s"Processing '$path'")
     try Right(fs2.io.file.readAll[Task](Paths.get(path), 4096)
       .through(text.utf8Decode)  // TODO: abstract file reading routines
       .runLog.map { _.mkString }.unsafeRun())
@@ -99,7 +101,7 @@ object Main {
 
   object include extends Plugin {
     def apply(s: String, cfg: Json): Option[String] = { // TODO: DRY
-      val pattern = """\{%\s*include\s*(.*)\s*%\}""".r
+      val pattern = """\{%\s+include\s+(.*)\s+%\}""".r
       val found   = pattern.findAllIn(s).nonEmpty
       if (found)
         Some( pattern.replaceAllIn(s, m => doInclude(m.group(1), cfg)) )
@@ -108,13 +110,13 @@ object Main {
 
     def doInclude(name: String, cfg: Json): String = (for {
       includesPath <- EitherT(Eval.later { cfg.hcursor.get[String]("includes").leftMap(_.getMessage) })
-      payload      <- readFile(s"/Users/anatolii/Projects/serenity/$workdirPath/$includesPath/$name")  // TODO: abstract workdirPath into the IO plugin
+      payload      <- readFile(s"$workdirPath/$includesPath/$name")  // TODO: abstract workdirPath into the IO plugin
     } yield payload).value.value.right.get  // TODO: No.
   }
 
   object variable extends Plugin {
     def apply(s: String, cfg: Json): Option[String] = {
-      val pattern = """\{\{\s*(.*)\s*\}\}""".r
+      val pattern = """\{\{\s+(.*)\s+\}\}""".r
       val found   = pattern.findAllIn(s).nonEmpty
       if (found)
         Some( pattern.replaceAllIn(s, m => doVar(m.group(1), cfg)) )
@@ -130,15 +132,42 @@ object Main {
     highlighter, include, variable
   ))
 
+  // TODO: layout logic goes to a separate plugin
+  def layout(name: String, content: String): Stuff[String] = for {
+    cfg             <- config
+    layoutsPath     <- EitherT(Eval.later { cfg.hcursor.get[String]("layouts").leftMap(_.getMessage) })
+    layoutCfg        = cfg.deepMerge( Json.fromJsonObject(JsonObject.singleton("content", Json.fromString(content))) )
+    layoutRaw       <- readFile(s"$workdirPath/$layoutsPath/$name.html")
+    (headerCfg, lt)  = maybeSplit(layoutRaw)
+    liquified       <- solid(lt, layoutCfg)
+    result          <- (for {
+                      hcfg    <- headerCfg
+                      hlayout <- hcfg.hcursor.get[String]("layout").toOption
+                    } yield layout(hlayout, liquified)).getOrElse(point[Stuff, String](liquified))
+  } yield result
+
+  def maybeSplit(str: String): (Option[Json], String) = if (str.startsWith("---\n")) {  // TODO: should return Eval (or maybe it is good as is?)
+    val lines     = str.split("\n").toList.drop(1)
+    val cfg       = yamlParser.parse(lines.takeWhile(_ != "---").mkString("\n")).toOption
+    val remainder = lines.dropWhile(_ != "---").drop(1).mkString("\n")
+    (cfg, remainder)
+  } else (None, str)
+
+  def point[M[_], A](a: A)(implicit M: Monad[M]): M[A] = M.pure(a)  // TODO: Similar lifts: S ~> G if S :<: G - introduce types a la carte.
+
   def main(args: Array[String]): Unit = {
     val processPosts: Stuff[Unit] = for {
       cfg        <- config
       postsPath  <- EitherT(Eval.later { cfg.hcursor.get[String]("posts"      ).leftMap(_.getMessage) })  // TODO: Abstract this mess
       outputPath <- EitherT(Eval.later { cfg.hcursor.get[String]("destination").leftMap(_.getMessage) })  // TODO: Abstract this mess
 
-      post       <- readFile (s"$workdirPath/$postsPath/$postName.md")
+      // TODO: IMPORTANT: this is actually processFile(pathOnTheLineBelow) - read the file with optional YAML config, use that config in `solid`, if there's a `layout` entry in the config, lay it out. Repeat untill nothing changes.
+      // TODO: How should the local yaml config be related to the liquid tags of the enclosing templates? Where this config is needed? It contains some metadata - tags, categories etc - so it will probably be needed from other plugins...
+      post       <- readFile(s"$workdirPath/$postsPath/$postName.md")  // TODO: readFileWithOptionalYamlConfigHeader
       liquified  <- solid(post, cfg)
-      _          <- writePost(liquified, s"$workdirPath/$outputPath/blog/$postName.html")
+      laidOut    <- layout("post", liquified)  // TODO: IMPORTANT: turns out, the layout is specified explicitly for posts too...
+      
+      _          <- writePost(laidOut, s"$workdirPath/$outputPath/blog/$postName.html")
     } yield ()
 
     println(processPosts.value.value)
